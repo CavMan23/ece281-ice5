6b7e0f6a73b070dbf0468fe74a9d5c4dff025827
cavman23 <141964392+CavMan23@users.noreply.github.com>
2024-04-04 08:48:43 -0600

    tb edit

--- src/hdl/elevator_controller_fsm.vhd
+++ src/hdl/elevator_controller_fsm.vhd
@@ -99,12 +99,12 @@ begin
     f_Q_next <= s_floor2 when (f_Q = s_floor1 and i_up_down = '1' and i_stop = '0') else -- going up
                 s_floor3 when (f_Q = s_floor2 and i_up_down = '1' and i_stop = '0') else
                 s_floor4 when (f_Q = s_floor3 and i_up_down = '1' and i_stop = '0') else
-                s_floor4 when (f_Q = s_floor4 and i_up_down = '1' and i_stop = '0') else
+                --s_floor4 when (f_Q = s_floor4 and i_up_down = '1' and i_stop = '0') else
                 -- going down
                 s_floor3 when (f_Q = s_floor4 and i_up_down = '0' and i_stop = '0') else -- going up
                 s_floor2 when (f_Q = s_floor3 and i_up_down = '0' and i_stop = '0') else
                 s_floor1 when (f_Q = s_floor2 and i_up_down = '0' and i_stop = '0') else
-                s_floor1 when (f_Q = s_floor1 and i_up_down = '0' and i_stop = '0') else
+               -- s_floor1 when (f_Q = s_floor1 and i_up_down = '0' and i_stop = '0') else
                 f_Q; -- default case
   
 	-- Output logic
